cf610555330873f99dc34b7d4a24d56a89cf0592
gabrielam0ura <gabrielamoura@alu.ufc.br>
2025-01-30 20:45:56 -0300

    Initial commit on front branch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <img src="/images/Navbar.png" alt="Navbar" className="navbar-image" />
+      <div className="split left">
+        <div className="camera"></div>
+        <p>CAMERA</p>
+      </div>
+      <div className="split right">
+        <p>CHESS</p>
+        <img src="/images/image 2.png" alt="Piece" className="piece-right-image" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
